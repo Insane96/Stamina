48c2d59d694e66c04afdd5444a2cec013d97f5ae
Insane96 <delvillano.alberto@gmail.com>
2026-01-15 13:35:50 +0100

    Expanded `/insanestamina` command: added `regen` and `consume` subcommands.

--- src/main/java/insane96mcp/stamina/command/SCommand.java
+++ src/main/java/insane96mcp/stamina/command/SCommand.java
@@ -13,24 +13,45 @@ import net.minecraft.server.level.ServerPlayer;
 public class SCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("insanestamina").requires(source -> source.hasPermission(2))
-            .then(Commands.literal("set")
-                    .then(Commands.argument("players", EntityArgument.players())
-                        .then(Commands.argument("amount", FloatArgumentType.floatArg(0))
+                .then(Commands.literal("set")
+                        .then(Commands.argument("players", EntityArgument.players())
+                                .then(Commands.argument("amount", FloatArgumentType.floatArg(0))
+                                        .executes(context -> {
+                                            for (ServerPlayer player : EntityArgument.getPlayers(context, "players")) {
+                                                StaminaHandler.setStamina(player, FloatArgumentType.getFloat(context, "amount"));
+                                                StaminaSync.sync(player);
+                                            }
+                                            return 1;
+                                        }))))
+                .then(Commands.literal("regen")
+                        .then(Commands.argument("players", EntityArgument.players())
+                                .then(Commands.argument("amount", FloatArgumentType.floatArg(0))
+                                        .executes(context -> {
+                                            for (ServerPlayer player : EntityArgument.getPlayers(context, "players")) {
+                                                StaminaHandler.regenStamina(player, FloatArgumentType.getFloat(context, "amount"));
+                                                StaminaSync.sync(player);
+                                            }
+                                            return 1;
+                                        }))))
+                .then(Commands.literal("consume")
+                        .then(Commands.argument("players", EntityArgument.players())
+                                .then(Commands.argument("amount", FloatArgumentType.floatArg(0))
+                                        .executes(context -> {
+                                            for (ServerPlayer player : EntityArgument.getPlayers(context, "players")) {
+                                                StaminaHandler.consumeStamina(player, FloatArgumentType.getFloat(context, "amount"));
+                                                StaminaSync.sync(player);
+                                            }
+                                            return 1;
+                                        }))))
+                .then(Commands.literal("get")
+                        .then(Commands.argument("player", EntityArgument.player())
                                 .executes(context -> {
-                                    for (ServerPlayer player : EntityArgument.getPlayers(context, "players")) {
-                                        StaminaHandler.setStamina(player, FloatArgumentType.getFloat(context, "amount"));
-                                        StaminaSync.sync(player);
-                                    }
-                                    return 1;
-                                }))))
-            .then(Commands.literal("get")
-                    .then(Commands.argument("player", EntityArgument.player())
-                    .executes(context -> {
-                        ServerPlayer player = EntityArgument.getPlayer(context, "player");
-                        float stamina = StaminaHandler.getStamina(player);
-                        context.getSource().sendSuccess(()
-                                -> Component.translatable("commands.insanestamina.get", player.getName(), stamina), false);
-                        return (int) stamina;
-                    }))));
+                                    ServerPlayer player = EntityArgument.getPlayer(context, "player");
+                                    float stamina = StaminaHandler.getStamina(player);
+                                    context.getSource().sendSuccess(()
+                                            -> Component.translatable("commands.insanestamina.get", player.getName(), stamina), false);
+                                    return (int) stamina;
+                                })))
+        );
     }
 }
